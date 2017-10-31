9c9d5d8b173cb91118d73ff7422e3ad55287f521
usuario <usuario@10.22.148.14>
2017-10-31 18:03:22 +0000

    version2;Selección lineal con intercambio

--- src/practica/pkg1/Ordenar1Vector.java
+++ src/practica/pkg1/Ordenar1Vector.java
@@ -2,50 +2,23 @@ import java.util.*;
 
 public class Ordenar1Vector implements OrdenarVector {
     public String nombreMetodo (){
-        return "Inserción lineal";
+        return "Selección Lineal con Intercambio.";
     }
-    public void ordena (int[] v, DatosEstadisticos de){
-        /**int aux=0;
-        int j=0;
-        for (int i=0;i<v.length-1;i++){
-            if (v[i]>v[i+1]){
-                aux = v[i];
-                v[i] = v[i+1];
-                v[i+1]=aux;
-                j=i-1;
-                while (j>=0 && v[j]>v[j+1]){
-                    aux=v[j];
-                    v[j]=v[j+1];
-                    v[j+1]=aux;
-                }
-            } 
-        }**/
+    public void ordena (int[] v, DatosEstadisticos de){        
+        int menor,pos,i,j;
         long startTime = System.nanoTime();
-        int aux;
-        for(int i=0;i<v.length-1;i++) {
-            for(int j=0;j<v.length-1-i;j++) {
-                if (v[j]>v[j+1]) {
-                    aux=v[j];
-                    v[j]=v[j+1];
-                    v[j+1]=aux;
+        for(i=0;i<v.length-1;i++) {
+            menor=v[i];
+            pos=i;
+            for(j=i+1;j<v.length;j++) {
+                if (menor>v[j]) {                    
+                    menor=v[j];
+                    pos=j;
                 }
             }
-        }
+            v[pos]=v[i];
+            v[i]=menor;
+        }        
         de.timeData = System.nanoTime() - startTime;
     }
 }
-
-        /**int[] ordenado = new int[v.length];
-        int j=0;
-        for (int i=1;i<v.length;i++){
-            while ((j<i) && (ordenado[j] <= v[i])){
-                j++;
-            }
-            for (int k=i-1;k<j;k++){
-                ordenado[k+1] = ordenado[k];
-            }
-            ordenado[j] = v[i];
-        }
-        for (int i=0;i<v.length;i++){
-            v[i] = ordenado[i];
-        }**/
